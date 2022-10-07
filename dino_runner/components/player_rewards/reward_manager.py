1ffdd1b648690074bfadc4123c3b611eb878a814
Daniel Virreira <Daniel.Virreira@fundacion-jala.org>
2022-10-07 17:53:36 -0400

    Clase faltante

--- dino_runner/components/player_rewards/reward_manager.py
+++ dino_runner/components/player_rewards/reward_manager.py
@@ -19,24 +19,10 @@ class RewardManager:
         if len(self.rewards) == 0:
             if(option == 0):
                 self.rewards.append(HalfHeart(x_position, y_position))
-
             else:
                 self.rewards.append(Chronometer(x_position, y_position))  
             self.has_reward = True
-
-        self.start_time_cd = pygame.time.get_ticks()
-        self.show_message(self.duration)
-
-    def show_message(self, duration):
-        show_message_time_up = self.start_time_cd + (duration * 1000)
-        time_to_show = round((show_message_time_up - pygame.time.get_ticks()) / 1000, 2)
-        print(time_to_show)
-        if time_to_show >= 0:
-            text = TextAlt("You've earned a power" , 22, 700, 40)
-            text.draw(self.screen)
-        else:
-            self.has_reward = False
-            self.rewards.pop()
+        
 
     def draw(self, screen):
         reward = self.rewards[0]
